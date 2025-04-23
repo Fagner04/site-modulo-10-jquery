fe1b1b049ccaff8f6a32665a65c6c5ade2037a66
Fagner04 <Fagnerc04@gmail.com>
2025-04-23 19:21:16 -0300

    Exercicio do modulo 10 ✔

--- js/main.js
+++ js/main.js
@@ -1,42 +1,70 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        // arrows: true, // Exibe as setas de navegação
+        autoplay: true, // Ativa o autoplay
+        autoplaySpeed: 3000, // Define o tempo entre slides
+        // dots: true // Mostra os indicadores de navegação
+    });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+
+            highlight: function (element) {
+                $(element).addClass("error").removeClass("valid");
+            },
+            unhighlight: function (element) {
+                $(element).addClass("valid").removeClass("error");
+            },
+           
         },
-        email: {
-            requerido: true,
-            email: true
+        messages: {
+            nome: 'Preenche o campo',
+            telefone:'Preenche o campo',
+            email:'Preenche um email válido',
+            telefone: 'Preenche o campo',
+            endereco: 'Preenche o campo',
+            cep: 'Preenche o campo',
+            cpf: 'Preenche o campo',
+
         },
-        telefone: {
-            requerido: true
+
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
+            submitHandler: function (form) {
+                alert("Formulário enviado com sucesso!");
+                form.reset();
+            }
+    })
 })
